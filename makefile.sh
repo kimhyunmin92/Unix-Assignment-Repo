e3ed416dfd94171c7ace7f6fb5690d8ff2b2d3d6
kimhyunmin92 <quahweijia@gmail.com>
2022-08-18 23:27:51 +0800

    sixth commit

--- makefile.sh
+++ makefile.sh
@@ -6,7 +6,9 @@ echo "Makefile - Coursera - Guessing Game Program (Quah Wei Jia)"
 lineNumber=`cat guessinggame.sh | wc -l`
 touch README.md
 echo "Coursera - Guessing Game Program (Quah Wei Jia)" >> README.md
+echo -e "\n" >> README.md
 echo "Make run date:" $currentDate >> README.md
+echo -e "\n" >> README.md
 echo "Total lines in Guessing Game Program:" $lineNumber >> README.md
 
 echo "-------- End of Program ---------------"
